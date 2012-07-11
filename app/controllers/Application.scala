509c923d900ae7eed7269a4150ba570c7425846e
Jan Helwich <jan.helwich@gmail.com>
2012-07-12 00:00:04 +0200

    next facebook connect step

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,55 +1,7 @@
 package controllers
 
 import play.api._
-import libs.ws.WS
-import play.api.mvc._
-import com.codahale.jerkson._
-import collection.immutable.ListMap
-import scala.Predef._
-
-trait FacebookAuth extends Controller{
-  val facebookClientId:String
-  val facebookClientSecret:String
-  val uriToRedirectFromFacebook:String
-  val scopes:String
-
-  lazy val facebookConnectClientUrl = "https://www.facebook.com/dialog/oauth?client_id="+facebookClientId+"&redirect_uri="+uriToRedirectFromFacebook+"&scope="+scopes+"&state=JANSTEST_ARBITRARY_BUT_UNIQUE_STRING_TEMP_TILL_GENERATED_AND_CHECKED"
-  lazy val facebookOauthTokenRequestUrl = "https://graph.facebook.com/oauth/access_token?client_id="+facebookClientId+"&redirect_uri="+uriToRedirectFromFacebook+"&client_secret="+facebookClientSecret+"&code="
-  lazy val facebookOauthTokenParseRegex = "access_token=(.*)\\&expires=".r
-  lazy val facebookGraphUrl = "https://graph.facebook.com/me?access_token="
-
-  //TODO scope params
-
-  def callFBforOauthTokenImmediatly(codeFromFB: String): String = {
-    val fbOathResult = WS.url(facebookOauthTokenRequestUrl + codeFromFB).get.value.get.body
-    val matchOfFBAuth = facebookOauthTokenParseRegex.findFirstMatchIn(fbOathResult)
-    matchOfFBAuth.get.subgroups(0)
-  }
-
-  case class FacebookAuthResult(val token:String){
-    lazy val graphResult = {
-      val basicInfo = WS.url(facebookGraphUrl + token).get.value.get.body
-      Json.parse[Map[String, String]](basicInfo)
-    }
-  }
-
-  def onSuccess(callback: FacebookAuthResult  => Result): FacebookAuthFailImpl = {
-    new FacebookAuthFailImpl(callback)
-  }
-
-  class FacebookAuthFailImpl(val callback: FacebookAuthResult  => Result){
-    def onFail(onFail: => Result): Action[AnyContent] = Action { implicit request =>
-      if(request.queryString.contains("code")){
-        val codeFromFB = request.queryString("code").head
-        val oauthToken = callFBforOauthTokenImmediatly(codeFromFB)
-        callback(new FacebookAuthResult(oauthToken))
-      } else {
-        onFail
-      }
-    }
-  }
-
-}
+import mvc._
 
 object Application extends Controller with FacebookAuth {
   val uriToRedirectFromFacebook = "http://janstest.de:9000/facebookauth"
@@ -65,12 +17,14 @@ object Application extends Controller with FacebookAuth {
     }
   }
 
-  def facebookauth = onSuccess{ result =>
-    println("YEAAAAHHH " + result.token)
-    Redirect("http://janstest.de:9000").withSession(("user", result.graphResult("name")))
-  } onFail (
-    Unauthorized("Please grant access to our app via facebook")
-  )
+  def facebookauth = authenticate{ result =>
+    result match {
+      case success: Success => {
+        Redirect("http://janstest.de:9000").withSession(("user", success.graphResult("name")))
+      }
+      case fail:Fail => Unauthorized("Please grant access to our app via facebook")
+    }
+  }
 
   def map = Action {
     Ok(render("map.scaml"))
---
+++ app/controllers/FacebookAuth.scala
@@ -0,0 +1,46 @@
+package controllers
+
+import play.api.mvc.{Action, Result, Controller}
+import play.api.libs.ws.WS
+import com.codahale.jerkson.Json
+
+trait FacebookAuth extends Controller {
+  val facebookClientId: String
+  val facebookClientSecret: String
+  val uriToRedirectFromFacebook: String
+  val scopes: String
+
+  lazy val facebookConnectClientUrl = "https://www.facebook.com/dialog/oauth?client_id=" + facebookClientId + "&redirect_uri=" + uriToRedirectFromFacebook + "&scope=" + scopes + "&state=JANSTEST_ARBITRARY_BUT_UNIQUE_STRING_TEMP_TILL_GENERATED_AND_CHECKED"
+  lazy val facebookOauthTokenRequestUrl = "https://graph.facebook.com/oauth/access_token?client_id=" + facebookClientId + "&redirect_uri=" + uriToRedirectFromFacebook + "&client_secret=" + facebookClientSecret + "&code="
+  lazy val facebookOauthTokenParseRegex = "access_token=(.*)\\&expires=".r
+  lazy val facebookGraphUrl = "https://graph.facebook.com/me?access_token="
+
+  def callFBforOauthTokenImmediatly(codeFromFB: String): String = {
+    val fbOathResult = WS.url(facebookOauthTokenRequestUrl + codeFromFB).get.value.get.body
+    val matchOfFBAuth = facebookOauthTokenParseRegex.findFirstMatchIn(fbOathResult)
+    matchOfFBAuth.get.subgroups(0)
+  }
+
+  case class AuthResult()
+
+  case class Success(val token: String) extends AuthResult() {
+    lazy val graphResult = {
+      val basicInfo = WS.url(facebookGraphUrl + token).get.value.get.body
+      Json.parse[Map[String, String]](basicInfo)
+    }
+  }
+
+  case class Fail() extends AuthResult()
+
+  def authenticate(callback: AuthResult => Result) = Action {
+    implicit request =>
+      if (request.queryString.contains("code")) {
+        val codeFromFB = request.queryString("code").head
+        val oauthToken = callFBforOauthTokenImmediatly(codeFromFB)
+        callback(Success(oauthToken))
+      } else {
+        callback(Fail())
+      }
+  }
+
+}
